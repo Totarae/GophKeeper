4245bf00d57f6601cb06bf91ece14af7f33be8fe
grigorii.ivanov <gregoryivanoff6@gmail.com>
2025-07-24 23:22:59 +0300

    CLIent

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"GophKeeper/internal/client/app"
+	"GophKeeper/internal/common/buildlog"
+)
+
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
+func main() {
+	buildlog.Print(buildVersion, buildDate, buildCommit)
+
+	app, err := app.New()
+	if err != nil {
+		panic(err)
+	}
+
+	app.Run()
+}
